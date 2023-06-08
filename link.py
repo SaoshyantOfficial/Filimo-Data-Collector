b770307f6edf492031dc14e796eb22a399a23ab8
Saleh.mh <salehmhosseini2002@gmail.com>
2023-06-08 16:21:55 +0330

    add some screenshots

--- link.py
+++
@@ -1,9 +0,0 @@
-"""saving movies and series links to gather their data"""
-
-import json
-
-links = ["https://www.filimo.com/m/vef58", "https://www.filimo.com/m/l1uef", "https://www.filimo.com/m/121775"]
-
-with open("links.json", "w") as f:
-    json.dump(links, f)
-f.close()
