7df616d5f5680a6dc8700fe5d5b4a6e7d02baa19
pasha <pasha.adel2@gmail.com>
2023-06-12 09:08:38 +0330

    interface and scraper changes

--- filimo.py
+++ filimo.py
@@ -1,98 +1,84 @@
-"""Web scraper to collect filimo.com reviews"""
 "Saoshyant"
+"""Web scraper to collect filimo.com reviews"""
 
 #import statements
-import pandas as pd
+import csv
+import logging
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-import time
-import pandas as pd
-import json
-import logging
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
-# Log options to handle errors
-# logging.basicConfig(filename='errors.log', filemode="w", format='occurred date & time: %(asctime)s %(levelname)-8s \nfile name: %(filename)s\nline number: %(lineno)d \nerror: %(message)s',
-#      level=logging.ERROR, datefmt='%Y-%m-%d %H:%M:%S')
+# Log file settings
+logging.basicConfig(filename='errors.log', filemode="w", format='occurred date & time: %(asctime)s %(levelname)-8s \nfile name: %(filename)s\nline number: %(lineno)d \nerror: %(message)s',
+datefmt='%Y-%m-%d %H:%M:%S')
 
-class Filimo():
-    
-    opinion_list = []
-    
-    def __init__(self, movie_links):
-         
-         
-         # Due to any unwanted error, in each loop the data frame is saved and loop's(which is the index of the-
-        # last item in the list that it's data has been collected) number will be shown;
-        # So if any error occurs during scraping, all the gathered data wont be lost and user can continue from-
-        # Where it has been stopped.
-        
-        self.movie_links = movie_links
-        
-        # driver oprion
-        driver = self.driver_options()
+class Filimo:
+    def __init__(self, movie_urls):
+        self.review_list = []
+        self.movie_urls = movie_urls
+        self.error = False
 
-        # Wait settings
+    def save_reviews_to_csv(self, review_list):
+        # Save reviews to a CSV file
+        with open('reviews.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerows([[review] for review in review_list])
+
+    def get_webdriver(self):
+        # Set up Firefox webdriver with headless mode
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Firefox(options=options)
+        # Set 15 seconds for page loading
+        # driver.set_page_load_timeout(15)
+
+        return driver
+
+    def collect_reviews(self):
+        driver = self.get_webdriver()
         wait = WebDriverWait(driver, 10)
 
-        # Index of the lst item in the list that it's data has been collected
-        last_index = 0
-        
-        driver.get(self.movie_links)
-
-        while True:
+        for url in self.movie_urls:
             try:
-                # click on show more to see all comments
-                show_more_btn = driver.find_element(By.CLASS_NAME, "loadmore-link")
-                # scroll to the element using JavaScript
-                driver.execute_script("arguments[0].scrollIntoView({ behavior: 'auto', block: 'center', inline: 'center' });", show_more_btn)
-                show_more_btn.click()
-                time.sleep(0.5)
+                driver.get(url)
 
+                while True:
+                    try:
+                        # Click on "Show More" button to see all comments
+                        show_more_btn = driver.find_element(By.CLASS_NAME, "loadmore-link")
+                        driver.execute_script("arguments[0].scrollIntoView({ behavior: 'auto', block: 'center', inline: 'center' });", show_more_btn)
+                        show_more_btn.click()
+                        time.sleep(0.5)
+                    except NoSuchElementException:
+                        break
+                    except Exception as e:
+                        logging.exception(f"Error while clicking 'Show More' button: {str(e)}")
+
+                # Get comment links
+                comments = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "comment-content")))
+
+                # Get comment text
+                for comment in comments:
+                    self.review_list.append(comment.text)
+
+            except TimeoutException as e:
+                logging.exception(f"Timed out while waiting for comments to load: {str(e)}")
             except Exception as e:
-                logging.error(e)
-                break
+                logging.exception(f"Error while collecting reviews: {str(e)}")
+                self.error = True
 
-
-            # comment links
-            comments = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "comment-content")))
-            
-            # Comment texts
-            for comment in comments:
-                self.opinion_list.append(comment.text)
-                
-            
-            
-        self.save_to_csv(self.opinion_list)
-        
-        # Closing driver
+        self.save_reviews_to_csv(self.review_list)
         driver.quit()
 
 
-
-    def save_to_csv(self , opinion_list):
-          #save the opinions
-          with open('opinions.csv', 'w') as csvfile:
-               writer = csv.writer(csvfile)
-               
-               for element in self.opinion_list:
-                    writer.writerow([element])
-
-    def driver_options(self):
-
-        # Selenium options
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--headless')
-        driver = webdriver.Firefox()
-        # Set 15 seconds for page loading
-        driver.set_page_load_timeout(15)
-
-        return driver
     
-# instance = Filimo('https://www.filimo.com/m/132691')
-        
+# a = ["https://www.filimo.com/m/os712"]
+# instance = Filimo(a)
+# instance.collector()
        
 
 
--- user_interface.py
+++ user_interface.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+
 import filimo
 
 app = Flask(__name__)
@@ -11,7 +12,6 @@ def index():
 # Route for handling the form submission
 @app.route('/submit', methods=['POST'])
 def submit():
-     
     # Get the URLs from the form
     urls = []
     for key, value in request.form.items():
@@ -19,15 +19,19 @@ def submit():
             urls.append(value)
 
     # Print the URLs
-    print(urls)
+    print(f"Received the following URLs: {urls}")
    
-
-
-    for i in urls:
-         instance = filimo.Filimo(i) 
-     
-    # Redirect to a success page or render a template with the URLs
+    # Collect data from the URLs
+    try:
+        filimo_instance = filimo.Filimo(urls) 
+        filimo_instance.collect_reviews()
+    except Exception as e:
+        error_message = f"An error occurred: {str(e)}"
+        return render_template('errorPage.html', message=error_message), 500
+    
+    # Display the results
     return render_template('result_page.html', urls=urls)
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
